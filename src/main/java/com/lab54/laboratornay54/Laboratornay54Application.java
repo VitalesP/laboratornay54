54fdff9276b15171ad011d0c6576d888b908dd08
Vital <vitalesperegudov@gmail.com>
2021-01-30 12:54:11 +0600

    Lesson 54: create project

---
+++ src/main/java/com/lab54/laboratornay54/Laboratornay54Application.java
@@ -0,0 +1,13 @@
+package com.lab54.laboratornay54;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratornay54Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboratornay54Application.class, args);
+    }
+
+}
