daf199e372c7eb6297844c2a36752777caa5eed8
Vital <vitalesperegudov@gmail.com>
2021-01-30 16:37:52 +0600

    Lesson 54: create two controller

---
+++ src/main/java/com/lab54/laboratornay54/controller/EventController.java
@@ -0,0 +1,10 @@
+package com.lab54.laboratornay54.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/event")
+public class EventController {
+
+}
