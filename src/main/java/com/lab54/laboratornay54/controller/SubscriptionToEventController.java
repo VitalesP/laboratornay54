964cc48a91c9b56d00debb9162f3caae738ed344
Vital <vitalesperegudov@gmail.com>
2021-02-01 19:08:57 +0600

    Lesson 54: create ALL lab. вроде всё работает

--- src/main/java/com/lab54/laboratornay54/controller/SubscriptionToEventController.java
+++ src/main/java/com/lab54/laboratornay54/controller/SubscriptionToEventController.java
@@ -2,11 +2,15 @@ package com.lab54.laboratornay54.controller;
 
 import com.lab54.laboratornay54.model.SubscriptionToEvent;
 import com.lab54.laboratornay54.modelDTO.AnswerDTO;
+import com.lab54.laboratornay54.modelDTO.SubscriptionToEventDto;
 import com.lab54.laboratornay54.service.SubscriptionToEventService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/subscription")
@@ -30,12 +34,12 @@ public class SubscriptionToEventController {
         return ResponseEntity.notFound().build();
     }
 
-    @GetMapping("/byEmail")
-    public ResponseEntity<AnswerDTO> getSubscribeToEventForEmail(@RequestParam String email){
+    @GetMapping("/byEmail/{email}")
+    public ResponseEntity<?> getSubscribeToEventForEmail(@PathVariable String email){
         if(subscriptionToEventService.existEmail(email)){
-            return ResponseEntity.ok().body(AnswerDTO.builder()
-                    .subscriptionToEventId(subscriptionToEventService.findByEmail(email).toString())
-                    .build());
+            var subscribtions=subscriptionToEventService.findByEmail(email);
+            var subscribtionsDto=subscribtions.stream().map(SubscriptionToEventDto::from).collect(Collectors.toList());
+            return ResponseEntity.ok(subscribtionsDto);
         } else
             return ResponseEntity.badRequest().body(AnswerDTO.builder()
                     .textAnswer("Такой почты нет.")
